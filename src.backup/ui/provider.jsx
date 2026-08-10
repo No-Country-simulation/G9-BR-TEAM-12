9d602afbccaaad524483fa0375196f8e30d7eb58
dev-corvus <3666808@alunouninter.com>
2026-08-10 15:24:22 -0300

    Remove src.backup; finalize frontend replacement

--- src.backup/ui/provider.jsx
+++
@@ -1,9 +0,0 @@
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
-
-export function Provider(props) {
-  return (
-    <ChakraProvider value={defaultSystem}>
-      {props.children}
-    </ChakraProvider>
-  )
-}
